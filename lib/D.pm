43b1a9fad6cc34611745f4db6713adef25bbcf19
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-11-09 11:33:13 +0900

    remove module dependency

--- lib/D.pm
+++ lib/D.pm
@@ -5,7 +5,10 @@ use strict;
 use warnings;
 
 use Data::Dumper;
-use Data::Recursive::Encode;
+use Encode ();
+use Carp ();
+use Scalar::Util qw(blessed refaddr);
+use B;
 
 require Exporter;
 
@@ -17,18 +20,78 @@ our $VERSION = '0.01';
 
 # Encode data to UTF-8 and dump it to STDERR using warn function adn Data::Dumper.
 sub du {
-  my $ref_data = \(@_);
-  $ref_data = Data::Recursive::Encode->encode_utf8($ref_data);
+  my ($ref_data) = @_;
+  $ref_data = _encode('UTF-8', $ref_data);
   my $ret = Dumper $ref_data;
   warn "$ret\n";
 }
 
 # Encode data to cp932 and dump it to STDERR using warn function adn Data::Dumper.
 sub dw {
-  my $ref_data = \(@_);
-  $ref_data = Data::Recursive::Encode->encode("cp932",$ref_data);
+  my ($ref_data) = @_;
+  $ref_data = _encode("cp932",$ref_data);
   my $ret = Dumper $ref_data;
   warn "$ret\n";
+}
+
+# Copy from Data::Recursive::Encode
+our $DO_NOT_PROCESS_NUMERIC_VALUE = 0;
+sub _apply {
+    my $code = shift;
+    my $seen = shift;
+ 
+    my @retval;
+    for my $arg (@_) {
+        if(my $ref = ref $arg){
+            my $refaddr = refaddr($arg);
+            my $proto;
+ 
+            if(defined($proto = $seen->{$refaddr})){
+                 # noop
+            }
+            elsif($ref eq 'ARRAY'){
+                $proto = $seen->{$refaddr} = [];
+                @{$proto} = _apply($code, $seen, @{$arg});
+            }
+            elsif($ref eq 'HASH'){
+                $proto = $seen->{$refaddr} = {};
+                %{$proto} = _apply($code, $seen, %{$arg});
+            }
+            elsif($ref eq 'REF' or $ref eq 'SCALAR'){
+                $proto = $seen->{$refaddr} = \do{ my $scalar };
+                ${$proto} = _apply($code, $seen, ${$arg});
+            }
+            else{ # CODE, GLOB, IO, LVALUE etc.
+                $proto = $seen->{$refaddr} = $arg;
+            }
+ 
+            push @retval, $proto;
+        }
+        else{
+            push @retval, defined($arg) && (! $DO_NOT_PROCESS_NUMERIC_VALUE || ! _is_number($arg)) ? $code->($arg) : $arg;
+        }
+    }
+ 
+    return wantarray ? @retval : $retval[0];
+}
+ 
+# Copy from Data::Recursive::Encode
+sub _encode {
+    my ($encoding, $stuff, $check) = @_;
+    $encoding = Encode::find_encoding($encoding)
+        || Carp::croak("unknown encoding '$encoding'");
+    $check ||= 0;
+    _apply(sub { $encoding->encode($_[0], $check) }, {}, $stuff);
+}
+ 
+# Copy from Data::Recursive::Encode
+sub _is_number {
+    my $value = shift;
+    return 0 unless defined $value;
+ 
+    my $b_obj = B::svref_2object(\$value);
+    my $flags = $b_obj->FLAGS;
+    return $flags & ( B::SVp_IOK | B::SVp_NOK ) && !( $flags & B::SVp_POK ) ? 1 : 0;
 }
 
 1;
@@ -49,10 +112,10 @@ D - Provides utility functions to encode data and dump it to STDERR.
   # Reference data that contains decoded strings
   my $data = [{name => 'あ'}, {name => 'い'}];
   
-  # Encode strings to UTF-8 in data with Data::Recursive::Encode and dump it to STDERR using warn function and Data::Dumper.
+  # Encode all strings to UTF-8 in data and dump it to STDERR using warn function and Data::Dumper.
   du $data;
   
-  # Encode strings to cp932 in data with Data::Recursive::Encode and dump it to STDERR using warn function and Data::Dumper.
+  # Encode all strings to cp932 in data and dump it to STDERR using warn function and Data::Dumper.
   dw $data;
 
 =head1 DESCRIPTION
@@ -71,7 +134,7 @@ D module provides utility functions to encode data and dump it to STDERR.
 
 =item * Can wirte dump operation by onliner: useD;du $data; useD;dw $data;
 
-=item * Use Data::Recursive::Encode to encode data
+=item * Encode all strings in reference data
 
 =item * Use Data::Dumper::Dumper function to dump data
 
@@ -89,11 +152,11 @@ Export C<du> and C<dw> functions.
 
   du $data;
 
-Encode strings to UTF-8 in data with L<Data::Recursive::Encode> and dump it to STDERR using warn function and L<Data::Dumper>.
+Encode all strings in reference data to UTF-8 and dump it to STDERR using warn function and L<Data::Dumper>.
 
 =head2 dw
 
-Encode strings to cp932 in data with L<Data::Recursive::Encode> and dump it to STDERR using warn function and L<Data::Dumper>.
+Encode all strings in reference data to cp932 and dump it to STDERR using warn function and L<Data::Dumper>.
 
 =head1 Bug Report
 
