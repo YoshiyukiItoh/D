64c069cc4456b6a35c5a8bafc8b018e27c5427d1
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-11-09 10:44:11 +0900

    improve pod

--- lib/D.pm
+++ lib/D.pm
@@ -11,27 +11,11 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use D ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	du dw
-);
+our @EXPORT = qw(du dw);
 
 our $VERSION = '0.01';
 
-# Preloaded methods go here.
-# Encode reference to utf8 and output to STDERR.
+# Encode data to UTF-8 and dump it to STDERR using warn function adn Data::Dumper.
 sub du {
   my $ref_data = \(@_);
   $ref_data = Data::Recursive::Encode->encode_utf8($ref_data);
@@ -39,7 +23,7 @@ sub du {
   warn "$ret\n";
 }
 
-# Encode reference to cp932 and output to STDERR.
+# Encode data to cp932 and dump it to STDERR using warn function adn Data::Dumper.
 sub dw {
   my $ref_data = \(@_);
   $ref_data = Data::Recursive::Encode->encode("cp932",$ref_data);
@@ -48,47 +32,86 @@ sub dw {
 }
 
 1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+
+=encoding utf8
 
 =head1 NAME
 
 D - Encode Values In A Structure
 
 =head1 SYNOPSIS
-
+  
+  use utf8;
+  
+  # Export du and dw functions
   use D;
-  du($data);
-  dw($data);
+  
+  # Reference data that contains decoded strings
+  my $data = [{name => 'あ'}, {name => 'い'}];
+  
+  # Encode strings to UTF-8 in data with Data::Recursive::Encode and dump it to STDERR using warn function and Data::Dumper.
+  du $data;
+  
+  # Encode strings to cp932 in data with Data::Recursive::Encode and dump it to STDERR using warn function and Data::Dumper.
+  dw $data;
 
 =head1 DESCRIPTION
 
-D uses Data::Recoursive::Encode, encodes the structure and outputs it to STDERR.
+D module provides utility functions to encode data and dump it to STDERR.
 
-=head2 EXPORT
+=head1 FEATURES
 
-None by default.
+=over 2
 
+=item * Export C<du> and C<dw> functions. These function names are consist of two characters. Don't conflict debug command 'p'.
 
+=item * C<du> is a short name of "dump UTF-8"
+
+=item * C<dw> is a short name of "dump Windows cp932"
+
+=item * Can wirte dump operation by onliner: useD;du $data; useD;dw $data;
+
+=item * Use Data::Recursive::Encode to encode data
+
+=item * Use Data::Dumper::Dumper function to dump data
+
+=item * Use warn function to print STDERR
+
+=back
+
+=head1 EXPORT
+
+Export C<du> and C<dw> functions.
+
+=head1 FUNCTIONS
+
+=head2 du
+
+  du $data;
+
+Encode strings to UTF-8 in data with L<Data::Recursive::Encode> and dump it to STDERR using warn function and L<Data::Dumper>.
+
+=head2 dw
+
+Encode strings to cp932 in data with L<Data::Recursive::Encode> and dump it to STDERR using warn function and L<Data::Dumper>.
+
+=head1 Bug Report
+
+L<https://github.com/YoshiyukiItoh/D>
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Data::Dumper>, L<Data::Recursive::Encode>
 
 =head1 AUTHOR
 
 yoshiyuki ito, E<lt>yoshiyuki.ito.biz@gmail.comE<gt>
 
+Yuki Kimoto, E<lt>kimoto.yuki@gmail.comE<gt>
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2019 by yoshi
+Copyright (C) 2019 by yoshi, Yuki Kimoto
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.08.7 or,
